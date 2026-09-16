a960e32933aa18957139d9953f72da8e4e9d381d
ratna182 <byebyanbye@gmail.com>
2026-09-16 09:54:55 +0700

    fix: jalankan migration activity log saat deploy

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 # Run pending migrations
 prisma db execute --file prisma/migrations/20260914053000_cashier_session_boundaries/migration.sql --schema prisma/schema.prisma
 prisma db execute --file prisma/migrations/20260914_add_alamat_to_warung/migration.sql --schema prisma/schema.prisma
+prisma db execute --file prisma/migrations/20260916000000_activity_logs/migration.sql --schema prisma/schema.prisma
 
 # Generate Prisma Client
 prisma generate
